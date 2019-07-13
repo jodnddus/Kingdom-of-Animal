9c5f19af60fb20d16f8e937dcc6eeb48accfbefd
조웅연 <lockhost23@gmail.com>
2019-07-13 22:11:13 +0900

    App에 반영 및 스타일(Sticky Header구현) 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import AnimalContainer from './AnimalContainer';
+import dogData from './data/q1_dog.json';
+import catData from './data/q1_cat.json';
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <AnimalContainer dogs={dogData} cats={catData} />
     </div>
   );
 }
