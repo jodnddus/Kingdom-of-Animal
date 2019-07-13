b0a46a464a520e0b19765fbb5c915ffc8e59d02f
조웅연 <lockhost23@gmail.com>
2019-07-13 22:10:34 +0900

    컴포넌트 통합(조건부 렌더링, 이벤트 등록)

---
+++ src/AnimalContainer.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import List from './List';
+import Header from './Header';
+import { Animal } from './interface';
+
+interface Props {
+  dogs: Animal[];
+  cats: Animal[];
+}
+
+interface State {
+  tab: string;
+}
+
+class AnimalContainer extends React.Component<Props, State> {
+  constructor(p: Props) {
+    super(p);
+    this.state = {
+      tab: "cats"
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e: MouseEvent, tab: string) {
+    e.preventDefault();
+    this.setState({
+      tab: tab
+    })
+  }
+
+  render() {
+    return (
+      <div className="AnimalContainer">
+        <Header onClick={this.onClick} activeTab={this.state.tab} />
+        {
+          this.state.tab === "dogs"
+            ? <List animals={this.props.dogs} />
+            : <List animals={this.props.cats} />
+        }
+      </div>
+    );
+  }
+}
+
+export default AnimalContainer;
