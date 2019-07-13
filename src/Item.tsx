86aa4a7476744f49c9cccbeeaf450ae654935e47
조웅연 <lockhost23@gmail.com>
2019-07-13 22:09:37 +0900

    리스트, 아이템 구현

---
+++ src/Item.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Animal } from './interface';
+
+const Item: React.FC<Animal> = ({ _id, imageUrl, age, name, gender }) => {
+  return (
+    <div className="items">
+      <div className="info">
+        <p><b>{name}</b>&nbsp;({age})</p>
+        <p>{gender}</p>
+        <p>{_id}</p>
+      </div>
+      <div className="images">
+        <img src={imageUrl} alt="animal" />
+      </div>
+    </div>
+  );
+}
+
+export default Item;
---
+++ src/List.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Item from './Item';
+import { Animal } from './interface';
+
+interface Props {
+  animals: Animal[];
+}
+
+const List: React.FC<Props> = ({ animals }) => {
+  return (
+    <div className="animal-list">
+      {
+        animals.map(animal =>
+          <Item
+            _id={animal._id}
+            imageUrl={animal.imageUrl}
+            age={animal.age}
+            name={animal.name}
+            gender={animal.gender}
+          />
+        )
+      }
+    </div>
+  );
+}
+
+export default List;
