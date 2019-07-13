608b52172d17ef82bbd7beb7d3582691c98c5b8e
조웅연 <lockhost23@gmail.com>
2019-07-13 22:52:29 +0900

    헤더 한글화

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,19 +8,19 @@ interface Props {
 const Header: React.FC<Props> = ({ onClick, activeTab }) => {
   return (
     <div className="header">
-      <h1>scatterlab</h1>
+      <h1>ㅅㅋㅌㄹ</h1>
       <div className="animal-buttons">
         <button
           className={activeTab === "dogs" ? "animal-button active-animal-button" : "animal-button"}
           onClick={(e) => onClick(e, "dogs")}
         >
-          Dog
+          강아지
         </button>
         <button
           onClick={(e) => onClick(e, "cats")}
           className={activeTab === "cats" ? "animal-button active-animal-button" : "animal-button"}
         >
-          Cat
+          고양이
         </button>
       </div>
     </div>
