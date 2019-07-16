e85f29820b35a90ba6da20bb15ede2dc419ba75d
조웅연 <lockhost23@gmail.com>
2019-07-16 13:49:45 +0900

    App에 api 호출 함수 반영하고 state를 이용해서 관리하기.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import AnimalContainer from './components/AnimalContainer';
-import dogData from './data/q1_dog.json';
-import catData from './data/q1_cat.json';
+import * as api from './api';
+import { Animal } from './interface';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <AnimalContainer dogs={dogData} cats={catData} />
-    </div>
-  );
+interface State {
+  dog: Animal[];
+  cat: Animal[];
+}
+
+interface Props {
+  // none props
+}
+
+class App extends React.Component<Props, State> {
+  constructor(p: Props) {
+    super(p);
+    this.state = {
+      dog: [],
+      cat: []
+    }
+    this.getAnimal = this.getAnimal.bind(this);
+  }
+
+  getAnimal = async () => {
+    try {
+      const { dogData, catData } = await api.getAnimal('dog', 'cat');
+      console.log(dogData, catData);
+      this.setState({
+        dog: dogData.data,
+        cat: catData.data
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  componentDidMount() {
+    this.getAnimal();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AnimalContainer dogs={this.state.dog} cats={this.state.cat} />
+      </div>
+    );
+  }
 }
 
 export default App;
