1bc19a652127bb3e617cd1a49e25fc8c1c9b4e38
조웅연 <lockhost23@gmail.com>
2019-08-19 11:04:02 +0900

    react-lazyloading 라이브러리를 이용한 지연로딩(Lazy Loading) 구현

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import { Animal } from '../interface';
 
 const Item: React.FC<Animal> = ({ _id, imageUrl, age, name, gender }) => {
   return (
-    <div className="items">
-      <div className="info">
-        <p><b>{name}</b>&nbsp;({age})</p>
-        <p>{gender}</p>
-        <p>{_id}</p>
+    <LazyLoad height={200}>
+      <div className="items">
+        <div className="info">
+          <p><b>{name}</b>&nbsp;({age})</p>
+          <p>{gender}</p>
+          <p>{_id}</p>
+        </div>
+        <div className="images">
+          <img src={imageUrl} alt="animal" />
+        </div>
       </div>
-      <div className="images">
-        <img src={imageUrl} alt="animal" />
-      </div>
-    </div>
+    </LazyLoad>
   );
 }
 
